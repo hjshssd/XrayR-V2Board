492d1bb85fec2b011d4fd940186c3153fc93eb9f
hjshssd <141552046+hjshssd@users.noreply.github.com>
2023-09-27 00:03:35 +0800

    Update XrayR.sh

--- XrayR.sh
+++ XrayR.sh
@@ -6,7 +6,27 @@ yellow='\033[0;33m'
 plain='\033[0m'
 
 version="v1.0.0"
+install_certbot() {
+    # 安装 Certbot
+    if [[ x"${release}" == x"ubuntu" || x"${release}" == x"debian" ]]; then
+        apt-get -y install certbot
+    elif [[ x"${release}" == x"centos" ]]; then
+        yum -y install certbot
+    else
+        echo -e "${red}不支持的操作系统${plain}"
+        exit 1
+    fi
+}
 
+get_ssl_cert() {
+    # 获取 SSL 证书
+    echo -e "${green}开始获取 SSL 证书${plain}"
+    certbot certonly --standalone -d yourdomain.com -m your-email@example.com --agree-tos
+    if [[ $? != 0 ]]; then
+        echo -e "${red}获取 SSL 证书失败，请检查您的域名和邮箱配置${plain}"
+        exit 1
+    fi
+}
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}错误: ${plain} 必须使用root用户运行此脚本！\n" && exit 1
 
@@ -168,8 +188,7 @@ uninstall() {
 
 start() {
     check_status
-    if [[ $? == 0 ]];
-    then
+    if [[ $? == 0 ]]; then
         echo ""
         echo -e "${green}XrayR已运行，无需再次启动，如需重启请选择重启${plain}"
     else
@@ -282,7 +301,180 @@ update_shell() {
     fi
 }
 
-# ... （下面是之前的代码） ...
+# 0: running, 1: not running, 2: not installed
+check_status() {
+    if [[ ! -f /etc/systemd/system/XrayR.service ]]; then
+        return 2
+    fi
+    temp=$(systemctl status XrayR | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+    if [[ x"${temp}" == x"running" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+check_enabled() {
+    temp=$(systemctl is-enabled XrayR)
+    if [[ x"${temp}" == x"enabled" ]]; then
+        return 0
+    else
+        return 1;
+    fi
+}
+
+check_uninstall() {
+    check_status
+    if [[ $? != 2 ]]; then
+        echo ""
+        echo -e "${red}XrayR已安装，请不要重复安装${plain}"
+        if [[ $# == 0 ]]; then
+            before_show_menu
+        fi
+        return 1
+    else
+        return 0
+    fi
+}
+
+check_install() {
+    check_status
+    if [[ $? == 2 ]]; then
+        echo ""
+        echo -e "${red}请先安装XrayR${plain}"
+        if [[ $# == 0 ]]; then
+            before_show_menu
+        fi
+        return 1
+    else
+        return 0
+    fi
+}
+
+show_status() {
+    check_status
+    case $? in
+        0)
+            echo -e "XrayR状态: ${green}已运行${plain}"
+            show_enable_status
+            ;;
+        1)
+            echo -e "XrayR状态: ${yellow}未运行${plain}"
+            show_enable_status
+            ;;
+        2)
+            echo -e "XrayR状态: ${red}未安装${plain}"
+    esac
+}
+
+show_enable_status() {
+    check_enabled
+    if [[ $? == 0 ]]; then
+        echo -e "是否开机自启: ${green}是${plain}"
+    else
+        echo -e "是否开机自启: ${red}否${plain}"
+    fi
+}
+
+show_XrayR_version() {
+    echo -n "XrayR 版本："
+    /usr/local/XrayR/XrayR -version
+    echo ""
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+show_usage() {
+    echo "XrayR 管理脚本使用方法: "
+    echo "------------------------------------------"
+    echo "XrayR              - 显示管理菜单 (功能更多)"
+    echo "XrayR start        - 启动 XrayR"
+    echo "XrayR stop         - 停止 XrayR"
+    echo "XrayR restart      - 重启 XrayR"
+    echo "XrayR status       - 查看 XrayR 状态"
+    echo "XrayR enable       - 设置 XrayR 开机自启"
+    echo "XrayR disable      - 取消 XrayR 开机自启"
+    echo "XrayR log          - 查看 XrayR 日志"
+    echo "XrayR update       - 更新 XrayR"
+    echo "XrayR update x.x.x - 更新 XrayR 指定版本"
+    echo "XrayR install      - 安装 XrayR"
+    echo "XrayR uninstall    - 卸载 XrayR"
+    echo "XrayR version      - 查看 XrayR 版本"
+    echo "------------------------------------------"
+}
+
+show_menu() {
+    echo -e "
+  ${green}XrayR 后端管理脚本，${plain}${red}不适用于docker${plain}
+--- https://github.com/missuo/XrayR-V2Board ---
+  ${green}0.${plain} 修改配置
+————————————————
+  ${green}1.${plain} 安装 XrayR
+  ${green}2.${plain} 更新 XrayR
+  ${green}3.${plain} 卸载 XrayR
+————————————————
+  ${green}4.${plain} 启动 XrayR
+  ${green}5.${plain} 停止 XrayR
+  ${green}6.${plain} 重启 XrayR
+  ${green}7.${plain} 查看 XrayR 状态
+  ${green}8.${plain} 查看 XrayR 日志
+————————————————
+  ${green}9.${plain} 设置 XrayR 开机自启
+ ${green}10.${plain} 取消 XrayR 开机自启
+————————————————
+ ${green}11.${plain} 一键安装 bbr (最新内核)
+ ${green}12.${plain} 查看 XrayR 版本 
+ ${green}13.${plain} 升级维护脚本
+ ${green}14.${plain} 获取 SSL 证书
+ "
+ #后续更新可加入上方字符串中
+    show_status
+    echo && read -p "请输入选择 [0-14]: " num
+
+    case "${num}" in
+        # ...（之前的菜单项）
+        14) install_certbot && get_ssl_cert
+        ;;
+        *) echo -e "${red}请输入正确的数字 [0-14]${plain}"
+        ;;
+    esac
+}
+
+    case "${num}" in
+        0) config
+        ;;
+        1) check_uninstall && install
+        ;;
+        2) check_install && update
+        ;;
+        3) check_install && uninstall
+        ;;
+        4) check_install && start
+        ;;
+        5) check_install && stop
+        ;;
+        6) check_install && restart
+        ;;
+        7) check_install && status
+        ;;
+        8) check_install && show_log
+        ;;
+        9) check_install && enable
+        ;;
+        10) check_install && disable
+        ;;
+        11) install_bbr
+        ;;
+        12) check_install && show_XrayR_version
+        ;;
+        13) update_shell
+        ;;
+        *) echo -e "${red}请输入正确的数字 [0-12]${plain}"
+        ;;
+    esac
+}
+
 
 if [[ $# > 0 ]]; then
     case $1 in
